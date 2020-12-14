d198e3fd793f89d8bc1616daea5f55e4c8738978
Paradosboy <1057458756@outlook.com>
2020-12-14 17:14:16 +0800

    进度:加盐加密-验证

--- ssm_service/src/main/java/com/service/impl/UserServiceImpl.java
+++ ssm_service/src/main/java/com/service/impl/UserServiceImpl.java
@@ -3,6 +3,7 @@ package com.service.impl;
 import com.dao.UserDao;
 import com.domain.Sysuser;
 import com.service.UserService;
+import com.utils.MD5Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -10,6 +11,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -29,9 +31,9 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         Sysuser user = userDao.findByUsername(username);
         if (user != null) {
             Collection<GrantedAuthority> list = new ArrayList<>();
-            SimpleGrantedAuthority simpleGrantedAuthority=new SimpleGrantedAuthority("ROLE_USER");
+            SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_USER");
             list.add(simpleGrantedAuthority);
-            return new User(user.getUsername(), "{noop}" + user.getPassword(),list);
+            return new User(user.getUsername(), user.getPassword(), list);
         }
         return null;
     }
@@ -41,8 +43,17 @@ public class UserServiceImpl implements UserService, UserDetailsService {
         return userDao.findAll();
     }
 
+    @Autowired
+    BCryptPasswordEncoder passwordEncoder;
+
     @Override
     public void save(Sysuser user) {
+        String password = user.getPassword();
+        /*String md5 = MD5Utils.md5(password);
+        user.setPassword(md5);*/
+
+        String encode = passwordEncoder.encode(password);
+        user.setPassword(encode);
         userDao.save(user);
     }
 }
